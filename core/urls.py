a384fcfd83b76bd110db54c589339a55c5ed34bc
Felipe <feli2003garcia@gmail.com>
2025-02-21 08:49:55 -0300

    delete views

--- core/urls.py
+++ core/urls.py
@@ -20,11 +20,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
-from .views import  TestView
+
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', TestView.as_view())
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
